94c8d5ddc0ff2a5cb8182e0f6677bb17bfbecd07
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2023-08-10 17:23:56 +0600

    Second Commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace _NET_7_WEB_API___ENTITY_FRAMEWORK_JUMPSTART;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
